1bf4b8619e4665493f613c05044ad765b5467296
ayousef27 <ahmenero27@gmail.com>
2024-12-29 09:17:41 +0300

    form

--- src/components/AdminProductForm.jsx
+++ src/components/AdminProductForm.jsx
@@ -14,6 +14,40 @@ const AdminProductForm = () => {
     const { name, value } = e.target
     setProductData({...productData,[name]: value,})
   }
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      await createProduct(productData)
+      alert('Product added successfully!')
+      setProductData({
+        name: '',
+        description: '',
+        price: '',
+        imageUrl: '',
+        category: '',
+        stockQuantity: '',
+      })
+    } catch (error) {
+      console.error('Error adding product:', error)
+      alert('fail add product. Please try again.')
+    }
+  }
 
-  
+  return (
+    <form onSubmit={handleSubmit}>
+      <h2>Add New Product</h2>
+      <div>
+        <label>Name:</label>
+        <input
+          type="text"
+          name="name"
+          value={productData.name}
+          onChange={handleChange}
+          required
+        />
+      </div>
+      </form>
+  )
+
+
 }
