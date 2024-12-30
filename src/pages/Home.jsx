5eff2b7391295564c98a4a5020a7f63fb324ff0b
7soon66 <68373803+7soon66@users.noreply.github.com>
2024-12-30 12:14:11 +0300

    correct navigation of sign in

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,25 @@
 import { useNavigate } from 'react-router-dom'
 
 
-const Home = () => {
+const Home = ({user}) => {
   let navigate = useNavigate()
 
   return (
     <div className="home-container col">
-      
-
-      <section className="welcome-signin">
-        <button onClick={() => navigate('/signin')}>
-          Click Here To Start
-        </button>
-      </section>
+    {user ? ( // Check if user is logged in
+        <section className="welcome-user">
+          {/* Content for logged-in users */}
+          <h2>Welcome, {user.name}!</h2> {/* Display user's name */}
+          <p>Explore our products and services.</p>
+          {/* Add other content for logged-in users here */}
+        </section>
+      ) : ( // Content for non-logged-in users
+        <section className="welcome-signin">
+          <button onClick={() => navigate('/signin')}>
+            Click Here To Start
+          </button>
+        </section>
+      )}
     </div>
   )
 }
