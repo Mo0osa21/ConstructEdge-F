9bd0a4f3f400ef03fa60e602b6ca1ef450007a7e
unknown <mmxmox@hotmail.com>
2024-12-30 15:50:17 +0300

    finish handle change

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,2 +1,15 @@
 import { useState } from 'react'
 import { updateUser } from '../services/UserServices'
+
+const Profile = ({ user, setUser }) => {
+  const [formValues, setFormValues] = useState({
+    username: user?.username || '',
+    password: '',
+  })
+  const [message, setMessage] = useState('')
+
+  const handleChange = (e) => {
+    setFormValues({ ...formValues, [e.target.name]: e.target.value })
+  }
+
+  
